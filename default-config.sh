9abeb3d06801ec4d3cda361a72be9fb60e8dcedf
Laica Lunasys <laica.lunasys@gmail.com>
2025-03-19 17:55:33 +0900

    Update config

---
+++ default-config.sh
@@ -0,0 +1,32 @@
+export COMPOSE_BAKE=true
+# Server Config
+export USE_DEV="false"
+
+## NOTE: DO NOT EDIT (use for directory name)
+export SERVER_ID="lumina"
+export SERVER_PATH="/app/fivem/txData/${SERVER_ID}"
+
+# Database Config
+export MARIADB_VERSION="11.5.2"
+export MYSQL_ROOT_PASSWORD=""
+export MYSQL_DATABASE="lumina_gta"
+export MYSQL_USER="app"
+export MYSQL_PASSWORD=""
+
+export MYSQL_ADDRESS="localhost"
+export MYSQL_PORT="23306"
+
+export DATABASE_URL="mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_ADDRESS}:${MYSQL_PORT}/${MYSQL_DATABASE}?charset=utf8mb4"
+
+export FXSERVER_LICENSE_KEY=""
+
+export STEAM_API_KEY=""
+export SCREENSHOT_BASIC_TOKEN=""
+export FIVEMANAGE_API_KEY=""
+export TEBEX_SECRET=""
+
+# rcon
+export RCON_ENABLED="YES"
+export RCON_ADDRESS="localhost"
+export RCON_PORT="30120"
+export RCON_PASSWORD="changeme_for_fxc0n"
--- template/fivem-server/start.sh
+++ template/fivem-server/start.sh
@@ -2,38 +2,12 @@
 set -e
 cd "$(dirname "$0")" && _base=$PWD
 
-rewriteGlobal() {
-    local TARGET="$1"
-
-    echo "=> Rewrite parameters..."
-
-    for f in $(find . -mindepth 1 -maxdepth 2 -type f \
-        -and -name '*.yaml' \
-        -or -name '*.yml' \
-        -or -name '*.json' \
-        -or -name '*.txt' \
-        -or -name '*.ini' \
-        -or -name '*.cfg' \
-        -or -name '*.conf' \
-        -or -name '*.config' \
-        -or -name '*.properties' \
-    ); do
-        sed -i $f -e "s/!!SERVER_HOSTNAME!!/${SERVER_HOSTNAME}/g"
-        sed -i $f -e "s/!!SERVER_PROJECT_NAME!!/${SERVER_PROJECT_NAME}/g"
-        sed -i $f -e "s/!!SERVER_PROJECT_DESC!!/${SERVER_PROJECT_DESC}/g"
-    done
-}
-
-
-#rewriteGlobal /app/fivem/txData
-
 if [ -e "${SERVER_PATH}" ]; then
     cd ${SERVER_PATH}
 
-    if [ -e "Makefile" ]; then
-        make generate-secrets
-        make generate-metadata
-        make generate-inventory
+    if [ -e "autorun.sh" ]; then
+        echo ":: Running autorun.sh"
+        bash ./autorun.sh
     fi
 fi
 
