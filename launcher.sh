ede96ea00a2573ffa62e5b0aa5f1c92a9c4e0641
Laica Lunasys <laica.lunasys@gmail.com>
2026-04-09 17:08:07 +0900

    Add database-console

--- launcher.sh
+++ launcher.sh
@@ -2,11 +2,24 @@
 set -e
 cd "$(dirname "$0")"
 
+source config.env
+
+# Set container use
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+
 function compose() {
-    # Set container use
-    export USER_ID=$(id -u)
-    export GROUP_ID=$(id -g)
-    docker compose --env-file ./config.env $@
+    docker compose --env-file ./config.env "$@"
+}
+
+function _database_console() {
+    query="$*"
+
+    if [ "$query" = "" ]; then
+        compose run --rm -it mariadb /usr/bin/mariadb -u root -p${MARIADB_ROOT_PASSWORD} -P ${MARIADB_PORT}
+    else
+        compose run --rm -T mariadb /usr/bin/mariadb -u root -p${MARIADB_ROOT_PASSWORD} -P ${MARIADB_PORT} -v -e "$*"
+    fi
 }
 
 function _preexec() {
@@ -46,10 +59,6 @@ function _config() {
     compose config
 }
 
-function _compose() {
-    compose $@
-}
-
 function _start_database() {
     mkdir -p $PWD/local/mysql
 
@@ -71,6 +80,9 @@ if [ "$1" = "start" ]; then
     _start
 elif [ "$1" = "start-database" ]; then
     _start_database
+elif [ "$1" = "database-console" ]; then
+    shift 1
+    _database_console $@
 elif [ "$1" = "stop" ]; then
     _stop
 elif [ "$1" = "restart" ]; then
@@ -82,8 +94,8 @@ elif [ "$1" = "config" ]; then
     _config
 elif [ "$1" = "compose" ]; then
     shift 1
-    _compose $@
+    compose $@
 else
-    echo "Usage: launcher <start|start-database|stop|restart|build|config|compose>"
+    echo "Usage: launcher <start|start-database|database-console|stop|restart|build|config|compose>"
     exit 1
 fi
